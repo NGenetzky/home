723d3028bffdb3283471286b9f68f4bf594a0342
Nathan Genetzky <nathan@genetzky.us>
2021-07-18 10:14:36 -0500

    screenlayout: laptop plus 2 displaylink outputs

---
+++ .screenlayout/novo-laptop-top-left-two-displaylink.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-4-4 --off --output DVI-I-3-3 --off --output DVI-I-2-2 --mode 3840x2160 --pos 3840x1080 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1-1 --mode 3840x2160 --pos 0x1080 --rotate normal
